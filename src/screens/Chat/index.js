e03274f180e1d2ec1cd961c6fd736175d3185acf
Vitor Paes <paesvitor@outlook.com>
2019-11-21 08:00:42 -0300

    feat: Added scroll to bottom when a new message is received

--- src/screens/Chat/index.js
+++ src/screens/Chat/index.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   chatStoreRequest,
@@ -16,6 +16,7 @@ function Chat() {
   const userId = useSelector(state => state.auth.user._id);
   const dispatch = useDispatch();
   const classes = useStyles();
+  const messagesEndRef = useRef();
 
   function _handleMessage(message) {
     message.user._id !== userId && dispatch(chatStoreSuccess(message));
@@ -24,6 +25,14 @@ function Chat() {
   function _handleSubmit(text) {
     dispatch(chatStoreRequest(text));
   }
+
+  function _scrollToBottom() {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  useEffect(() => {
+    _scrollToBottom();
+  }, [chat.payload]);
 
   useMemo(() => {
     dispatch(chatIndexRequest());
@@ -37,6 +46,7 @@ function Chat() {
         {chat.payload.map(message => (
           <Message message={message} key={message._id} />
         ))}
+        <div ref={messagesEndRef} />
       </section>
 
       <SendBar onSubmit={_handleSubmit} />
